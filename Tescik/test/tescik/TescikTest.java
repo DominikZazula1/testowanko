61260732af2cbb4729023fc4832d01fb9e3f9c5f
Dominik <demotywatory@interia.pl>
2021-01-17 18:54:51 +0100

    9

--- Tescik/test/tescik/TescikTest.java
+++ Tescik/test/tescik/TescikTest.java
@@ -43,7 +43,12 @@ public class TescikTest {
 	System.out.println(System.getProperty("java.specification.version"));
 	assertTrue(JAVA_7_OR_EARLIER);
     }
-
+    @Test
+    public void testVersion1() {
+	final boolean JAVA_8_OR_EARLIER = System.getProperty("java.specification.version").compareTo("1.8") <= 0;
+	System.out.println(System.getProperty("java.specification.version"));
+	assertTrue(JAVA_8_OR_EARLIER);
+    }
     /**
      * Test of main method, of class Tescik.
      */
@@ -51,7 +56,7 @@ public class TescikTest {
     public void testMain() {
         System.out.println("main");
         String[] args = null;
-       Tescik.main(args);
+       //Tescik.main(args);
         //TODO review the generated test code and remove the default call to fail.
         //fail("Wygenerowano perfidny błąd!");
     }
